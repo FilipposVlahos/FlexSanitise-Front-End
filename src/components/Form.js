bc46642fe70b4965dc476eb8957733c5a3d4c683
Filippos Vlahos <filippos.vlahos@student.manchester.ac.uk>
2021-01-30 15:33:03 +0000

    Add multiple questions in Form
    Also added Bootstrap components for improved visual

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,59 @@
 import React, { useState } from "react";
 import sanitise from "../api/sanitise_api"
+import {Form as BootstrapForm, Button} from 'react-bootstrap';
+import "./Form.css"
 
 const LOADING = "loading";
 const RESULT = "result";
 
 function Form (props) {
     const [text, setText] = useState("");
-    const [questions, setQuestions] = useState([]);
+    // const [questions, setQuestions] = useState([]);
+    const [values, setValues] = useState({ questions: []});
+
+    function createInputs() {
+        return values.questions.map((el, i) =>
+            <div key={i} >
+                <BootstrapForm.Group controlId="exampleForm.ControlTextarea1">
+                    <BootstrapForm.Label>Question {i + 1} </BootstrapForm.Label>
+                    <BootstrapForm.Control as="textarea" rows={1} onChange={handleChange.bind(i)} required />
+                </BootstrapForm.Group>
+                <Button variant="danger" onClick={removeClick.bind(i)} >
+                    Remove Question {i + 1}
+                </Button>
+                {/* <input type="text" value={el || ''} onChange={handleChange.bind(i)} /> */}
+                {/* <input type='button' value='Remove Question' onClick={removeClick.bind(i)} /> */}
+            </div>
+        );
+    }
+
+    function handleChange(event) {
+        let vals = [...values.questions];
+        vals[this] = event.target.value;
+        setValues({ questions: vals });
+    }
+
+    const addClick = () => {
+        setValues({ questions: [...values.questions, ''] })
+    }
+
+    const removeClick = () => {
+        let vals = [...values.questions];
+        vals.splice(this, 1);
+        setValues({ questions: vals });
+    }
 
     async function handleSubmit() {
         props.setPage(LOADING);
-        sanitise(text, [questions])
-            .then(function (response) {
+        var questions = JSON.stringify(values);
+        console.log(questions)
+        sanitise(text, questions)
+            .then(response => {
                 props.setResponse(response.data.sanitisedDocument);
                 console.log(response.data.sanitisedDocument)
                 props.setPage(RESULT);
             })
-            .catch(function (error) {
+            .catch(error => {
                 if (error.response) {
                     console.log(error.response.status);
                     props.setError(error.response.status);
@@ -32,26 +69,28 @@ function Form (props) {
     }
 
     return(
-    <form onSubmit={handleSubmit}>
-        <label>
-            Document:
-        </label>
-        <input
-            type="text"
-            value={text}
-            onChange={e => setText(e.target.value)}
-        />
-        <label>
-            Questions:
-        </label>
-        <input
-            type="text"
-            value={questions}
-            onChange={e => setQuestions(e.target.value)}
-        />
-        <br /><br />
-        <input type="submit" value="Submit" />
-    </form>    
+    <BootstrapForm onSubmit={handleSubmit}>
+        <div className= "row">
+            <div className = "column left">
+                <BootstrapForm.Group controlId="exampleForm.ControlTextarea1">
+                    <BootstrapForm.Label>Text to be Sanitised</BootstrapForm.Label>
+                    <BootstrapForm.Control as="textarea" rows={18} onChange={e => setText(e.target.value)} required/>
+                </BootstrapForm.Group>
+            </ div>
+
+            <div className = "column right">
+                {createInputs()}
+                <br />
+                <Button variant="secondary" onClick={addClick} >
+                    Add Question
+                </Button>
+            </div>
+        </div>
+        <br />
+        <Button variant="primary" type="submit">
+                Submit
+        </Button>
+    </BootstrapForm>    
     );
 }
 
