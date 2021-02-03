06ef164ae5e83935583c3a9ebe8fa54cb3638aad
Filippos Vlahos <filippos.vlahos@student.manchester.ac.uk>
2021-02-03 15:24:53 +0000

    Download Result + CSS

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Alert, Button, InputGroup, FormControl } from 'react-bootstrap';
+import useWindowDimensions from "../useWindowDimension";
+import "./Result.css"
 
 const FORM = "form";
 
 function Result(props) {
+    const { height } = useWindowDimensions();
+
+    const downloadTxtFile = () => {
+        const element = document.createElement("a");
+        const file = new Blob([document.getElementById('sanitisedText').value], { type: 'text/plain' });
+        element.href = URL.createObjectURL(file);
+        element.download = "myFile.txt";
+        document.body.appendChild(element); // Required for this to work in FireFox
+        element.click();
+    }
+
     let result;
     if (props.response) {
         result = 
-        <InputGroup>
-            <InputGroup.Prepend>
-                </InputGroup.Prepend>
-                <FormControl as="textarea" aria-label="With textarea" 
-                defaultValue={props.response}
-                rows={21}/>
-        </InputGroup>;
+        <div>
+            <InputGroup>
+                <FormControl as="textarea" aria-label="With textarea" defaultValue={props.response} rows={height/35} id="sanitisedText"/>
+            </InputGroup>
+            <br/>
+            <Button variant="secondary" onClick={downloadTxtFile}>Download</Button>
+        </div>;
         
     } else if (props.error) {
         result =  
@@ -27,11 +40,14 @@ function Result(props) {
 
     return(
         <>
-            {result}
-            <br />
-            <Button onClick={() => props.setPage(FORM)} >
-                Main Page
-            </Button>
+            <div className="result">
+                {result}
+            </div>
+            <div className="result">
+                <Button onClick={() => props.setPage(FORM)} >
+                    Main Page
+                </Button>
+            </div>
         </>
     );
 
