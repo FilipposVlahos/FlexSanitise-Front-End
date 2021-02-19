692b3a7fccd9ad2661e6acec2f15ca313193ae54
Filippos Vlahos <filippos.vlahos@student.manchester.ac.uk>
2021-02-19 22:06:05 +0000

    Result shows both sanitised and highlighted document

--- src/components/Result.js
+++ src/components/Result.js
@@ -12,27 +12,39 @@ function Result(props) {
         const element = document.createElement("a");
         const file = new Blob([document.getElementById('sanitisedText').value], { type: 'text/plain' });
         element.href = URL.createObjectURL(file);
-        element.download = "myFile.txt";
+        element.download = "sanitised_document.txt";
         document.body.appendChild(element); // Required for this to work in FireFox
         element.click();
     }
 
     let result;
-    if (props.response) {
+    if (props.response != null && props.response.sanitisedDocument != null && props.response.highlightedDocument != null) {
+    let highlightedDocument = props.response.highlightedDocument.replace(/(?:\r\n|\r|\n)/g, '<br />');
+        const html = '<div style="text-align:left;">' + highlightedDocument + '</div>';
         result = 
         <div>
-            <InputGroup>
-                <FormControl as="textarea" aria-label="With textarea" defaultValue={props.response} rows={height/35} id="sanitisedText"/>
-            </InputGroup>
-            <br/>
-            <Button variant="secondary" onClick={downloadTxtFile}>Download</Button>
+            <div className="row">
+                <div className="column left">
+                    <h4>Sanitised Document</h4>
+                    <InputGroup>
+                        <FormControl as="textarea" aria-label="With textarea" defaultValue={props.response.sanitisedDocument} rows={height/35} id="sanitisedText"/>
+                    </InputGroup>
+                </div>
+                <div className="column right">
+                    <h4>Highlighted Document</h4>
+                    <div>
+                        <div dangerouslySetInnerHTML={{__html: html}}></div>
+                    </div>
+                </div>
+            </div>
+            <Button variant="secondary" onClick={downloadTxtFile}>Download Sanitised Document</Button>
         </div>;
     } else if (props.error) {
         result =  
         <Alert variant="danger" >
             <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
             <p>
-                Error code: {props.error}
+                {props.error}
             </p>
         </Alert>;
     }
