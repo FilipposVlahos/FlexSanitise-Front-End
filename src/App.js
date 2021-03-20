d183447ac23b6cfa95dd38c4fd7a686cac85f6ec
Filippos Vlahos <filippos.vlahos@student.manchester.ac.uk>
2021-03-20 17:14:09 +0000

    Changed the name of the app from Document Sanitiser to Text Sanitiser

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Document Sanitiser
+          Text Sanitiser
         </h1>
       </header>
       <div className="body">
